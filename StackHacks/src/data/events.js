cb7cb5a26624c1880db5d6504e09d453ee1ae030
EKim06 <ericskim06@gmail.com>
2025-12-22 11:06:05 -0500

    added stackhacks logo and mock data

---
+++ StackHacks/src/data/events.js
@@ -0,0 +1,16 @@
+import reactLogo from "../assets/react.svg"
+export const events = [
+  {
+    title: "Hackathon Workshop",
+    date: "Nov 22",
+    image: reactLogo,
+    description: "Learn React + APIs"
+    
+  },
+  {
+    title: "Study Night",
+    date: "Dec 3",
+    image: reactLogo,
+    description: "Come study with us!"
+  }
+];
